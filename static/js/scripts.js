768e33b5a68d263115273af77a6bec7cb589eadd
Hector Acosta <hacostacervantes17@mail.wou.edu>
2022-04-02 13:57:47 -0700

    Added scripts.js and style.css files.

---
+++ static/js/scripts.js
@@ -0,0 +1,44 @@
+console.log("test")
+//Handle form for signing up.
+$("form[name=signup_form]").submit(function(e){
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/user/signup",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp) {
+            window.location.href = "/dashboard/";
+        },
+        error: function(resp){
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
+
+//Handle form for login.
+$("form[name=login_form]").submit(function(e){
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/user/login",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp) {
+            window.location.href = "/dashboard/";
+        },
+        error: function(resp){
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
