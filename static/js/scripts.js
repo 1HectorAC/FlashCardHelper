5a39d786b43540f141c4d57d130d098aac03497e
Hector Acosta <hacostacervantes17@mail.wou.edu>
2022-04-25 14:02:21 -0700

    Added some form handling on adding a cards list in scripts.js.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,3 @@
-console.log("test")
 //Handle form for signing up.
 $("form[name=signup_form]").submit(function(e){
     var $form = $(this);
@@ -29,6 +28,28 @@ $("form[name=login_form]").submit(function(e){
 
     $.ajax({
         url: "/user/login",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp) {
+            window.location.href = "/dashboard/";
+        },
+        error: function(resp){
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
+
+// Handle form to add a cards list.
+$("form[name=addCards]").submit(function(e){
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/user/addCards",
         type: "POST",
         data: data,
         dataType: "json",
