e59a794b2c2b9a078da502fb7aa7da7975cdce72
Hector Acosta <hacostacervantes17@mail.wou.edu>
2022-05-20 14:01:10 -0700

    Added intro to display cards in viewCardsScrpts.js and viewCards.html.

--- static/js/viewCardsScript.js
+++ static/js/viewCardsScript.js
@@ -3,11 +3,18 @@ var cardsSize = 0;
 var questionSide = true;
 var flashCards = [];
 
-// Sets the flash card variable and related variable.
-function setFlashCards(cards){
-
-    flashCards = cards;
-    cardsSize = cards.length;
+// Setup up some cards variables and view of card realated items in html.
+function startCardView(cards){
+    flashCards = cards
+    cardsSize = flashCards.length;
+    $('#problemLabel').text("Question");
+    $('#problemText').text(flashCards[index].question);
+    // Enable next next button if there is more than one card in the flashCards.
+    if(flashCards.length > 1)
+        $('#nextButton').prop('disabled', false)  
+    // Setup display if cards.
+    $('#introCard').hide();
+    $('#displayCard').show();
 }
 
 //Sets up change of diplayed question or answer text in html.
@@ -28,17 +35,12 @@ function cardFlip(){
 function nextCard(){
     questionSide = true;
     index = ++index;
-    console.log(index)
     // Disable next button if index reaches end of cardsSize.
-    if(index +1 >= cardsSize){
-        console.log("disable next button");
+    if(index +1 >= cardsSize)
         $('#nextButton').prop('disabled', true)
-    }
     // Enable previous button if we move after first card.
-    if(index == 1){
-        console.log("enable previous button")
+    if(index == 1)
         $('#previousButton').prop('disabled', false)
-    }
     // Update text to show question.
     if(index <= cardsSize){
         $('#problemLabel').text("Question");
@@ -50,17 +52,12 @@ function nextCard(){
 function previousCard(){
     questionSide = true;
     index = --index;
-    console.log(index)
     // Disable previous button if index reaches 0.
-    if(index == 0){
-        console.log("disable next button");
+    if(index == 0)
         $('#previousButton').prop('disabled', true)
-    }
     // Enable next button if we move away from final card.
-    if(index == cardsSize - 2){
-        console.log("enable previous button")
+    if(index == cardsSize - 2)
         $('#nextButton').prop('disabled', false)
-    }
     // Update text to show question.
     if(index <= cardsSize){
         $('#problemLabel').text("Question");
