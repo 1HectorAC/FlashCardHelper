0222de695aa7b0fea7388222db37eee9526de109
Hector Acosta <hacostacervantes17@wou.edu>
2022-06-27 16:34:05 -0700

    Added flip questions/answers function in viewCardsScript.js and its display in viewCards.html.

--- static/js/viewCardsScript.js
+++ static/js/viewCardsScript.js
@@ -4,13 +4,17 @@ var questionSide = true;
 var flashCards = [];
 
 // Setup up some cards variables and view of card realated items in html.
-function startCardView(cards){
+function startCardView(cards) {
     flashCards = cards
     cardsSize = flashCards.length;
 
-    //Shuffle cards check.
-    if($('#shuffleCheck').prop('checked'))
+    // Shuffle cards check.
+    if ($('#shuffleCheck').prop('checked'))
         flashCards = shuffleArray(flashCards);
+
+    // Flip questions with answers.
+    if ($('#flipCheck').prop('checked'))
+        flashCards = flipQuestionsAndAnswers(flashCards);
 
     //Edit some text.
     $('#index').text(1);
@@ -19,8 +23,8 @@ function startCardView(cards){
     $('#problemText').text(flashCards[index].question);
 
     // Enable next next button if there is more than one card in the flashCards.
-    if(flashCards.length > 1)
-        $('#nextButton').prop('disabled', false)  
+    if (flashCards.length > 1)
+        $('#nextButton').prop('disabled', false)
 
     // Setup display if cards.
     $('#introCard').hide();
@@ -30,31 +34,30 @@ function startCardView(cards){
 }
 
 //Sets up change of diplayed question or answer text in html.
-function cardFlip(){
+function cardFlip() {
     questionSide = !questionSide;
-    if(questionSide){
+    if (questionSide) {
         $('#problemLabel').text("Question");
         $('#problemText').text(flashCards[index].question);
     }
-    else{
+    else {
         $('#problemLabel').text("Answer");
         $('#problemText').text(flashCards[index].answer);
     }
-
 }
 
 // Show next card in html.
-function nextCard(){
+function nextCard() {
     questionSide = true;
     index = ++index;
     // Disable next button if index reaches end of cardsSize.
-    if(index +1 >= cardsSize)
+    if (index + 1 >= cardsSize)
         $('#nextButton').prop('disabled', true)
     // Enable previous button if we move after first card.
-    if(index == 1)
+    if (index == 1)
         $('#previousButton').prop('disabled', false)
     // Update text to show question.
-    if(index <= cardsSize){
+    if (index <= cardsSize) {
         $('#index').text(index + 1);
         $('#problemLabel').text("Question");
         $('#problemText').text(flashCards[index].question);
@@ -62,17 +65,17 @@ function nextCard(){
 }
 
 // Show previous card in html.
-function previousCard(){
+function previousCard() {
     questionSide = true;
     index = --index;
     // Disable previous button if index reaches 0.
-    if(index == 0)
+    if (index == 0)
         $('#previousButton').prop('disabled', true)
     // Enable next button if we move away from final card.
-    if(index == cardsSize - 2)
+    if (index == cardsSize - 2)
         $('#nextButton').prop('disabled', false)
     // Update text to show question.
-    if(index <= cardsSize){
+    if (index <= cardsSize) {
         $('#index').text(index + 1);
         $('#problemLabel').text("Question");
         $('#problemText').text(flashCards[index].question);
@@ -84,13 +87,23 @@ function shuffleArray(array) {
     let curId = array.length;
     // There remain elements to shuffle
     while (0 !== curId) {
-      // Pick a remaining element
-      let randId = Math.floor(Math.random() * curId);
-      curId -= 1;
-      // Swap it with the current element.
-      let tmp = array[curId];
-      array[curId] = array[randId];
-      array[randId] = tmp;
+        // Pick a remaining element
+        let randId = Math.floor(Math.random() * curId);
+        curId -= 1;
+        // Swap it with the current element.
+        let tmp = array[curId];
+        array[curId] = array[randId];
+        array[randId] = tmp;
     }
     return array;
-  }
+}
+
+// flip answers and questions in array.
+function flipQuestionsAndAnswers(array) {
+    for (let i = 0; i < array.length; i++) {
+        temp = array[i].question;
+        array[i].question = array[i].answer;
+        array[i].answer = temp;
+    }
+    return array;
+}
